948fe26059e2d2e7b1af08ec2a5c182b27a045b6
altronis <jihu1011@gmail.com>
2020-08-30 23:21:18 -0400

    Refactor: Improve logic for handling escape sequences

--- utility.py
+++ utility.py
@@ -8,8 +8,23 @@ def format_hex(x: int) -> str:
 
 # Handle escape sequences and replace them with the actual characters
 def handle_escapes(s: str) -> str:
-    s = re.sub(r'\\n', '\n', s)
-    s = re.sub(r'\\t', '\t', s)
-    s = re.sub(r'\\0', '\0', s)
+    escape_seqs = {
+        'n': '\n',
+        't': '\t',
+        '0': '\0',
+        '"': '"'
+    }
+
+    for escape_char in escape_seqs:
+        def replace_func(match):
+            # Replace the backslash and the following character with the replacement
+            return match.group(0)[:-2] + escape_seqs[escape_char]
+
+        # Match an odd number of backslashes followed by the escape character
+        pattern = r'(?<!\\)(\\\\)*\\' + escape_char
+        s = re.sub(pattern, replace_func, s)
+
+    # Replace all double backslashes with a single backslash (since \\ is an escape seq for \)
+    s = re.sub(r'\\\\', r'\\', s)
 
     return s
