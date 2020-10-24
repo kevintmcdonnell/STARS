bc920b308c03dfcd3a9ce3d3cd0f7f016977e55c
altronis <jihu1011@gmail.com>
2020-10-23 21:08:51 -0400

    Allow apostrophe character literal

--- lexer.py
+++ lexer.py
@@ -124,7 +124,7 @@ class MipsLexer(Lexer):
         t.value = int(t.value, 0)
         return t
 
-    @_(r"'(.|\s|\\[0rnt])'")
+    @_(r"'(\\[0rnt']|.|\s)'")
     def CHAR(self, t):
         char = t.value[1: -1]
 
@@ -136,6 +136,8 @@ class MipsLexer(Lexer):
             char = '\r'
         elif char == '\\t':
             char = '\t'
+        elif char == "\\'":
+            char = '\''
 
         t.value = ord(char)
         return t
